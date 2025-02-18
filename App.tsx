72cfbb094f53e7939f228fe9700f3fd8633111a8
Igor Ferreira <igorkmail@gmail.com>
2025-02-17 23:03:46 -0300

    Greetings

--- App.tsx
+++ App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-const HelloWorldApp = () => {
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+  },
+});
+
+type GreetingProps = {
+  name: string;
+};
+
+const Greeting = (props: GreetingProps) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
+    <View style={styles.center}>
+      <Text>Hello {props.name}!</Text>
     </View>
   );
 };
-export default HelloWorldApp; 
+
+const LotsOfGreetings = () => {
+  return (
+    <View style={[styles.center, { top: 50 }]}>
+      <Greeting name="Rexxar" />
+      <Greeting name="Jaina" />
+      <Greeting name="Valeera" />
+    </View>
+  );
+};
+
+export default LotsOfGreetings;
