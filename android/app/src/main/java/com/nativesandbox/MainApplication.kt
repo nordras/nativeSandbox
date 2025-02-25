2dd569f43d0912deedd08d5db1b4243d9b388ff4
Igor Ferreira <igorkmail@gmail.com>
2025-02-24 23:56:56 -0300

    initialize flipper

--- android/app/src/main/java/com/nativesandbox/MainApplication.kt
+++ android/app/src/main/java/com/nativesandbox/MainApplication.kt
@@ -35,9 +35,41 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // Se a nova arquitetura estiver ativada, carregamos a entrada nativa
       load()
+    }
+  }
+
+  /**
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+   *
+   * @param context
+   * @param reactInstanceManager
+   */
+  private static void initializeFlipper(
+      Context context, ReactInstanceManager reactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes
+         Flipper, since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.example.ReactNativeFlipper");
+        aClass
+            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+            .invoke(null, context, reactInstanceManager);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
