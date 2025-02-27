3c9a477dc572fc3d20bc86ed993b3440fc336225
Igor Ferreira <igorkmail@gmail.com>
2025-02-27 14:01:35 -0300

    Make cached image

--- src/pages/Counter.tsx
+++ src/pages/Counter.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import FastImage from 'react-native-fast-image';
+
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
     <View style={styles.container}>
+      <FastImage
+        style={styles.image}
+        source={{
+          uri,
+          priority: FastImage.priority.high, // Dá prioridade alta para o carregamento
+        }}
+        resizeMode={FastImage.resizeMode.cover} // Ajusta a imagem ao container
+      />
       <Text>You clicked {count} times</Text>
       <Button
         onPress={() => setCount(count + 1)}
@@ -21,6 +31,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
   },
 });
 
